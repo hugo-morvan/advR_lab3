4ea61d7cbab01da905dfb3b67c68b586add1d48c
Hugo Morvan <98044151+hugo-morvan@users.noreply.github.com>
2023-09-17 22:31:22 +0200

    sync

--- advRlab3/R/euclidean.R
+++ advRlab3/R/euclidean.R
@@ -1,5 +1,16 @@
-euclidean <-
-function(input1, input2){
+#' Euclidean's Algorithmm
+#'
+#' @description This function calculates the greatest common divisor, that is the
+#' number that divides both inputs without remainder
+#' @param input1 a numeric scalar
+#' @param input2 a numeric scalar
+#' @return The function returns the greatest common divisor of the given two inputs
+#' @seealso \href{https://en.wikipedia.org/wiki/Euclidean_algorithm}{Euclidean Algorithm}
+#' @export
+#' @examples
+#' euclidean(123612, 13892347912)
+
+euclidean <- function(input1, input2){
   # Asserting that inputs are numeric scalars
   stopifnot(
     is.numeric(input1), is.vector(input1), length(input1) == 1,
