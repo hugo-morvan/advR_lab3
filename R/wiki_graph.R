ebd4d95a68485534947f33d4296be7ddd9c2b0e2
Hugo Morvan <98044151+hugo-morvan@users.noreply.github.com>
2023-09-18 11:16:18 +0200

    updated data description

--- R/wiki_graph.R
+++ R/wiki_graph.R
@@ -1,14 +1,14 @@
 #' Wikipedia graph
 #'
-#' A connected graph from the wikipedia page of Dijkstra'a algorithm meant to test the functionning of the dijkstra function in this package.
+#' A dataset containing a connected graph from the wikipedia page of Dijkstra'a algorithm meant to test the functionning of the dijkstra function in this package.
 #'
 #'
-#' @format ## `wiki_graph`
+#' @format wiki_graph
 #' A data frame with 3 columns:
 #' \describe{
 #'   \item{v1}{from node}
 #'   \item{v2}{to node}
-#'   \item{w}{weight}
+#'   \item{w}{weight of the connection}
 #' }
-#' @source <https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm>
+#' @source https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm/
 "wiki_graph"
